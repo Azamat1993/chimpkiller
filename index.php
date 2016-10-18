f9706affc21ca17185a2462362c2a782902c7ade
Azamat Zorkanov <openitkz@gmail.com>
2016-10-18 16:13:12 +0600

    a

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once('config/app.php');
 
 
 
+
 if(file_exists($requested_file=str_replace(SUB_URL,'',strtok($_SERVER['REQUEST_URI'],'?').'.php'))) {
 	$protect=isset($middlewares[str_replace('.php','',$requested_file)]) ? $middlewares[str_replace('.php','',$requested_file)] : null;
 	Guard::protect($protect);
